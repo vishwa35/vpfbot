f1de87c9872f6308d7a3b90fa6bd9b088181d8d9
Vishwa Shah <vshah0350@gmail.com>
2018-09-20 15:57:58 -0400

    fixed after wrong msg sent

--- slack_balances.py
+++ slack_balances.py
@@ -30,18 +30,40 @@ if __name__ == "__main__":
       if mem[2] == "Collegiate":
         print '\n'
         print mem
-        if mem[3] == '0.00':
+        if mem[3] == '0.00' or mem[3] == '0':
+
+          # msg = "Apologies for the useless message. You currently owe nothing. Thanks for paying your dues on time!"
+          # openChannel = slack_client.api_call(
+          #   "conversations.open",
+          #   users=slackids[mem[0]]
+          # )
+          # sendMsg = slack_client.api_call(
+          #   "chat.postMessage",
+          #   channel=openChannel["channel"]["id"],
+          #   # channel='#slackbot-test',
+          #   text=msg
+          # )
+
+          # if sendMsg['ok'] is not True:
+          #   logging.error(sendMsg)
+          # else:
+          #   logging.debug(sendMsg)
+
           logging.info("Zero balance: {}".format(mem[1]))
         elif mem[0] in slackids.keys():
         # elif mem[0] == '859480' and mem[0] in slackids.keys():
           print "executing"
           uid = slackids[mem[0]]
           amt = mem[3]
-          msg = "Time for Fall 2018 Dues! :flying_money_with_wings: You currently owe *${}*-- please venmo :venmo: @gtakpsi, pay via check, or pay on chapterspot.com (+3% transaction fee). Due August 30!\n>Note: If you venmo, you should receive a confirmation message within 30 (or so) minutes! If you think there is an error, please slack @vishwa. If you would like a payment plan, please fill out this form: https://goo.gl/forms/MXbsr1FnMu5obJQM2".format(amt)
+          firstduesmsg = "Time for Fall 2018 Dues! :flying_money_with_wings: You currently owe *${}*-- please venmo :venmo: @gtakpsi, pay via check, or pay on chapterspot.com (+3% transaction fee). Due August 30!\n>Note: If you venmo, you should receive a confirmation message within 30 (or so) minutes! If you think there is an error, please slack @vishwa. If you would like a payment plan, please fill out this form: https://goo.gl/forms/MXbsr1FnMu5obJQM2".format(amt)
+          # dayofmsg = "Fall 2018 Dues are due *TODAY*! :flying_money_with_wings: You currently owe *${}*-- please venmo :venmo: @gtakpsi, pay via check, or pay on chapterspot.com (+3% transaction fee).\n>Note: If you venmo, you should receive a confirmation message within 30 (or so) minutes! If you think there is an error, please slack @vishwa. If you would like a payment plan, please fill out this form: https://goo.gl/forms/MXbsr1FnMu5obJQM2".format(amt)
           # msg = "Time for Fall 2018 Dues! :flying_money_with_wings: You currently owe ${}-- please venmo :venmo: @gtakpsi, pay via check, or pay on chapterspot.com (+3% transaction fee). Due August 30!".format(amt)
+          
+          msg = dayofmsg
+
           openChannel = slack_client.api_call(
             "conversations.open",
-            users=uid
+            users=slackids[mem[0]]
           )
           sendMsg = slack_client.api_call(
             "chat.postMessage",
